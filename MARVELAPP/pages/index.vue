d728e84aa2dcc21082778a1eeb35ef46f8efe97b
Nagase <samael041203@gmail.com>
2023-09-26 15:25:30 -0500

    descripción de los personajes lista

--- MARVELAPP/pages/index.vue
+++ MARVELAPP/pages/index.vue
@@ -1,70 +1,84 @@
 <template>
-    <div class="character-container">
-      <div
-        v-for="character in characters"
-        :key="character.id"
-        class="carta"
-        @click="showCharacterDetails(character)"
-      >
-        <div class="linea-roja">
-          <h2 class="nombres">{{ character.name }}</h2>
-        </div>
-        <div class="contenido">
-          <div class="character-image-container">
-            <img
-              :src="character.thumbnail.path + '.' + character.thumbnail.extension"
-              alt=""
-              class="imagen"
-            />
-          </div>
-        </div>
-        
+  <div class="character-container">
+    <div v-for="character in characters" :key="character.id" class="carta" @click="showCharacterDetails(character)">
+      <div class="linea-roja">
+        <h2 class="nombres">{{ character.name }}</h2>
       </div>
-      <div v-if="selctedcharacter" class="modal">
-          <div v-if="selctedcharacter" class="modal-content">
-            
-            <!-- Contenido del modal -->
-            <h2>{{ selctedcharacter.name }}</h2>
-            <!-- Agrega aquí más detalles del personaje si es necesario -->
-            <button class="close-button" @click="closeModal()">Cerrar</button>
-          </div>
+      <div class="contenido">
+        <div class="character-image-container">
+          <img :src="character.thumbnail.path + '.' + character.thumbnail.extension" alt="" class="imagen" />
         </div>
-    </div>
-  </template>
-  
+      </div>
 
-  <script setup>
-  import { ref } from 'vue';
-  import axios from 'axios';
+    </div>
+    <div v-if="selctedcharacter" class="modal">
+      <div class="modal-content">
+        <center>
+        <h2>{{ selctedcharacter.name }}</h2>
+        <div class="selctedcharacter-image-container">
+          <img :src="selctedcharacter.thumbnail.path + '.' + selctedcharacter.thumbnail.extension" alt="" class="imagen" />
+        </div>
+        <p>{{ selctedcharacter.description }}</p>
+        <h3>Cantidad de cómics: {{ selctedcharacter.comics.available }}</h3>
+        <h3>Cantidad de series: {{ selctedcharacter.series.available }}</h3>
+        <h3>Cantidad de stories: {{ selctedcharacter.stories.available }}</h3>
+        <h3>Cantidad de events: {{ selctedcharacter.events.available }}</h3>
+        </center>
+        <h3 align="center">3 primeras series en la que aparece:</h3>
+        <ul>
+          <template v-for="(seriesItem, index) in selctedcharacter.series.items" :key="seriesItem.resourceURI">
+            <!-- Mostrar solo las primeras 3 series -->
+            <li v-if="index < 3">
+              {{ seriesItem.name }}
+            </li>
+          </template>
+        </ul>
+        <br>
+        <br>
+        <center><button class="close-button" @click="closeModal()">Cerrar</button></center>
+      </div>
+    </div>
+  </div>
+</template>
   
-  const Hash = 'c922a0d15cfa7035874464492f823891';
-  const publicKeys = '2dc534f2f94eb05c4aa8b32c092a6d02';
-  const characters = ref([]);
-  const selctedcharacter=ref(null);
-  
-  const loadCharacters = async () => {
-    const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=${publicKeys}&hash=${Hash}`;
-    const { data } = await axios.get(url);
-  
-    const charactersWithImages = data.data.results.filter((character) => {
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const Hash = 'c922a0d15cfa7035874464492f823891';
+const publicKeys = '2dc534f2f94eb05c4aa8b32c092a6d02';
+const characters = ref([]);
+const selctedcharacter = ref(null);
+
+const loadCharacters = async () => {
+  const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=${publicKeys}&hash=${Hash}`;
+  const { data } = await axios.get(url);
+
+  const charactersWithImages = data.data.results
+    .filter((character) => {
       return character.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available';
+    })
+    .map(async (character) => {
+      const characterDetails = await axios.get(
+        `https://gateway.marvel.com:443/v1/public/characters/${character.id}?apikey=${publicKeys}&hash=${Hash}`
+      );
+      character.description = characterDetails.data.data.results[0].description;
+      return character;
     });
-  
-    characters.value = charactersWithImages;
-    console.log(charactersWithImages);
-  };
-  
-  
-  
-  const showCharacterDetails = (character) => {
-    console.log('Mostrando detalles para el personaje:', character.name);
-    selctedcharacter.value=character
-  };
-  
-  const closeModal = () => {
-    selctedcharacter.value=null;
-  };
-  loadCharacters();
-  </script>
+
+  characters.value = await Promise.all(charactersWithImages);
+  console.log(characters.value);
+};
+
+const showCharacterDetails = (character) => {
+  console.log('Mostrando detalles para el personaje:', character.id);
+  selctedcharacter.value = character;
+};
+
+const closeModal = () => {
+  selctedcharacter.value = null;
+};
+loadCharacters();
+</script>
   
   
