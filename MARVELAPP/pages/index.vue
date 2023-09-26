18d788be7e8d40ff4c42b8c87b12824d3842e3e4
Nagase <samael041203@gmail.com>
2023-09-26 12:17:37 -0500

    Arreglado botón cerrar

--- MARVELAPP/pages/index.vue
+++ MARVELAPP/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="character-container">
-    <div
-      v-for="character in characters"
-      :key="character.id"
-      class="carta"
-      @click="showCharacterDetails(character)"
-    >
+      <div
+        v-for="character in characters"
+        :key="character.id"
+        class="carta"
+        @click="showCharacterDetails(character)"
+      >
         <div class="linea-roja">
           <h2 class="nombres">{{ character.name }}</h2>
         </div>
@@ -18,83 +18,21 @@
             />
           </div>
         </div>
-        <div :id="'modal-' + character.id" class="modal">
-        <div class="modal-content">
-          <span class="close" @click="closeModal(character.id)">&times;</span>
-          <!-- Contenido del modal -->
-          <h2>{{ character.name }}</h2>
-          <!-- Agrega aquí más detalles del personaje si es necesario -->
+        
+      </div>
+      <div v-if="selctedcharacter" class="modal">
+          <div v-if="selctedcharacter" class="modal-content">
+            
+            <!-- Contenido del modal -->
+            <h2>{{ selctedcharacter.name }}</h2>
+            <!-- Agrega aquí más detalles del personaje si es necesario -->
+            <button class="close-button" @click="closeModal()">Cerrar</button>
+          </div>
         </div>
-      </div>
-      </div>
     </div>
   </template>
   
-  <style scoped>
-  .character-container {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  
-  .carta {
-    width: 23%; /* Ajusta el ancho para que haya 4 tarjetas por fila */
-    margin: 10px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-  }
-  
-  .linea-roja {
-    border-bottom: 2px solid red;
-    margin-bottom: 10px;
-    padding-bottom: 5px;
-  }
-  
-  .imagen {
-    width: 100%; /* Haz que la imagen ocupe el 100% del contenedor */
-    height: 200px; /* Ajusta la altura según tus preferencias */
-    object-fit: cover; /* Ajusta la forma en que se muestra la imagen */
-  }
 
-  /* Agrega estilos para el modal */
-.modal {
-  display: none;
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.4);
-  padding-top: 60px;
-}
-
-.modal-content {
-  background-color: #fefefe;
-  margin: 5% auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-}
-
-  </style>
-  
   <script setup>
   import { ref } from 'vue';
   import axios from 'axios';
@@ -102,6 +40,7 @@
   const Hash = 'c922a0d15cfa7035874464492f823891';
   const publicKeys = '2dc534f2f94eb05c4aa8b32c092a6d02';
   const characters = ref([]);
+  const selctedcharacter=ref(null);
   
   const loadCharacters = async () => {
     const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=${publicKeys}&hash=${Hash}`;
@@ -115,18 +54,17 @@
     console.log(charactersWithImages);
   };
   
-  loadCharacters();
-
-  const showCharacterDetails = (character) => {
-    const modal = document.getElementById('modal-' + character.id);
-    modal.style.display = 'block';
-  };
-
-  const closeModal = (characterId) => {
-    const modal = document.getElementById('modal-' + characterId);
-    modal.style.display = 'none';
-  };
-
   
+  
+  const showCharacterDetails = (character) => {
+    console.log('Mostrando detalles para el personaje:', character.name);
+    selctedcharacter.value=character
+  };
+  
+  const closeModal = () => {
+    selctedcharacter.value=null;
+  };
+  loadCharacters();
   </script>
+  
   
